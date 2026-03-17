339d793d61a85e5f05e5fd44108a210e1c41bed9
Subham Pradhan <sp9925097@gmail.com>
2026-03-17 20:04:12 +0530

    Order status api added

--- backend1/src/main/java/com/ecommerce/backend1/controller/OrderController.java
+++ backend1/src/main/java/com/ecommerce/backend1/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.ecommerce.backend1.controller;
 
+import com.ecommerce.backend1.entity.Cart;
 import com.ecommerce.backend1.entity.Order;
+import com.ecommerce.backend1.repository.CartRepository;
 import com.ecommerce.backend1.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,14 +16,28 @@ public class OrderController {
     @Autowired
     private OrderRepository orderRepository;
 
-    @PostMapping
-    public Order publicOrder(@RequestBody Order order){
+    @Autowired
+    private CartRepository cartRepository;
+
+    @PostMapping("/place/{userId}")
+    public Order placeOrder(@PathVariable Long userId){
+        List<Cart> cartItems = cartRepository.findByUserId(userId);
+
+        double total = cartItems.stream()
+                .mapToDouble(c -> c.getProduct().getPrice())
+                .sum();
+        Order order = new Order();
+        order.setUser(cartItems.get(0).getUser());
+        order.setTotalAmount(total);
+        order.setStatus("PLACED");
+
+        cartRepository.deleteAll(cartItems);
+
         return orderRepository.save(order);
     }
-
-    @GetMapping
-    public List<Order> getAllOrders(){
-        return orderRepository.findAll();
+    @GetMapping("/user/{userId}")
+    public List<Order> getOrders(@PathVariable Long userId){
+        return orderRepository.findByUserId(userId);
     }
 }
 
--- backend1/src/main/java/com/ecommerce/backend1/repository/OrderRepository.java
+++ backend1/src/main/java/com/ecommerce/backend1/repository/OrderRepository.java
@@ -4,5 +4,9 @@ package com.ecommerce.backend1.repository;
 import com.ecommerce.backend1.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+List<Order> findByUserId(Long userId);
 }
